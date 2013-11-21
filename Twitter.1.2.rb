560fc9283c7cfae1fde151b37e0c0e5ea704ec22
RogerVega <rogervegarojas@gmail.com>
2013-11-21 00:39:57 -0600

    Función twitter con unos comentarios más

---
+++ Twitter.1.2.rb
@@ -0,0 +1,32 @@
+require 'oauth'
+require 'twitter'
+require 'launchy'
+
+def tweett(token_acceso,token_secreto) #Funcion que toma los valores secretos del usuario registrado
+@user=Twitter::REST::Client.new do |config|
+      config.consumer_key = 'x4pUZIkRRMRvCpVDJwdhw' #Codigo del usuario propietario de la aplicación
+      config.consumer_secret = 'D8yU5s5qzRHfnfSN6pZXXZEobqfn2dqGZ27HlwwOiI' #Codigo secreto del usuario propietario de la aplicación.
+      config.oauth_token = token_acceso #Token de acceso del usuario
+      config.oauth_token_secret = token_secreto #Token de acceso secreto del usuario (contraseña)
+    end
+    
+puts "Publicar un nuevo Tweet..."
+tweet= gets() #Obtenemos lo que el usuario digite
+@user.update(tweet) #Funcion que envía el tweet
+end
+#puts "Estas siendo dirigido a la página de autenticación"
+redirect_url = "http://ubuntulinuxcr.blogspot.com" #Dirige al usuario a la página de autenticación 
+	
+url = URI.parse(URI.encode(redirect_url.strip))
+consumer_key='x4pUZIkRRMRvCpVDJwdhw'
+consumer_secret='D8yU5s5qzRHfnfSN6pZXXZEobqfn2dqGZ27HlwwOiI'
+consumer = OAuth::Consumer.new(consumer_key, consumer_secret,
+	                             { :site => "http://api.twitter.com" })
+request_token = consumer.get_request_token#(:oauth_callback => url)
+Launchy.open request_token.authorize_url
+puts "Por favor ingrese el PIN que le fue dado cuando autorizo la aplicacion"
+pincode = gets.chomp #Se toma el PIN ingresado por el usuario el cual permite interactuar con su perfil de twitter
+access_token = request_token.get_access_token :oauth_verifier => pincode #Verificación del código de PIN
+puts access=access_token.token # user token
+puts secret=access_token.secret # user oauth secret
+tweett(access,secret) #Hace llamado a la función tweett con los datos del usuario que autorizo la aplicación
